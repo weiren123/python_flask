9febd2e299beb5c88decf8965d64ea2dd1e00459
15122042182@163.com <15122042182@163.com>
2018-04-01 22:45:43 +0800

    页面

--- flaskdemo/config.py
+++ flaskdemo/config.py
@@ -1,5 +1,8 @@
 #encoding:utf-8
-
+import os
+DEBUG = True
+# 随机产生24个字节的字符串
+SECRET_KEY = os.urandom(24)
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'test'
--- flaskdemo/flaskdemo.py
+++ flaskdemo/flaskdemo.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 from flask_sqlalchemy import SQLAlchemy
 import config
 app = Flask(__name__)
@@ -14,7 +14,7 @@ db.create_all()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 @app.route('/hello')
 def hello():
     # article = Article(title = 'aaa',content = 'bbb')
@@ -38,6 +38,6 @@ def startimage():
         'img': "https://dn-shimo-image.qbox.me/ZiiJ3jowWLEHccHq/image.png"
     }
     return jsonify(response), 200
-
+#
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
