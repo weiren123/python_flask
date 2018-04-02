59ab095762eb5da036db643e4dbdddf24dc88825
刘亚伟 <liuyawei@thinkyun.cn>
2018-04-02 16:20:13 +0800

    登录/注册页

--- flaskdemo/flaskdemo.py
+++ flaskdemo/flaskdemo.py
@@ -1,20 +1,31 @@
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 import config
 app = Flask(__name__)
 app.config.from_object(config)
-db = SQLAlchemy(app)
-
-class Article(db.Model):
-    __tablename__ = 'article'
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    title = db.Column(db.String(100),nullable=False)
-    content = db.Column(db.Text,nullable=False)
-db.create_all()
+# db = SQLAlchemy(app)
+# class Article(db.Model):
+#     __tablename__ = 'article'
+#     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+#     title = db.Column(db.String(100),nullable=False)
+#     content = db.Column(db.Text,nullable=False)
+# db.create_all()
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
+@app.route('/regist/',methods=['GET','POST'])
+def regist():
+    if request.method == 'GET':
+        return render_template('regist.html')
+    else:
+        pass
 @app.route('/hello')
 def hello():
     # article = Article(title = 'aaa',content = 'bbb')
@@ -25,9 +36,9 @@ def hello():
     # article = Article.query.filter(Article.title == 'aaa').first()
     # article.title = 'new title'
     # db.session.commit()
-    article = Article.query.filter(Article.title == 'aaa').first()
-    db.session.delete(article)
-    db.session.commit()
+    # article = Article.query.filter(Article.title == 'aaa').first()
+    # db.session.delete(article)
+    # db.session.commit()
 
     return 'Hello, World1111'
 @app.route('/startimage/image.png')
